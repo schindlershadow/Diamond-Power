7f8ceab00fd30c0ae6d9083f59951c94e47ac506
schindlershadow <schindlershadow@gmail.com>
2014-10-01 18:33:51 -0400

    changes to CustomArmor.java

--- src/main/java/com/schindlershadow/diamondpower/CustomArmor.java
+++ src/main/java/com/schindlershadow/diamondpower/CustomArmor.java
@@ -1,13 +1,23 @@
 package com.schindlershadow.diamondpower;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemArmor;
 
-public class CustomArmor extends ItemArmor{
+public class CustomArmor extends ItemArmor {
 
-	public CustomArmor(ArmorMaterial material, int par2,
-			int par3) {
+	public CustomArmor(ArmorMaterial material, int par2, int par3) {
 		super(material, par2, par3);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister) {
+		String itemName = getUnlocalizedName().substring(
+				getUnlocalizedName().lastIndexOf(".") + 1);
+		this.itemIcon = par1IconRegister.registerIcon(itemName);
 	}
 
 }
